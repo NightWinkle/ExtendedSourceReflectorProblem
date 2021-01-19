73b9ead5a821cb75d8e38fffdc9e66253c209925
Guillaume Chazareix <guillaume@chazareix.net>
2021-01-19 08:55:29 +0100

    Fix in interpolation

--- reflector_problem/reflector.py
+++ reflector_problem/reflector.py
@@ -2,8 +2,8 @@ from geomloss.sinkhorn_samples import clusterize
 
 def interpolate_potentials(sinkhorn_result, new_angular_support):
     softmin = sinkhorn_result.softmin
-    C_xy = (new_angular_support, sinkhorn_result.C_xy[1].detach())
-    C_xx = (new_angular_support, sinkhorn_result.identity.forward(sinkhorn_result.C_xx[0].detach()))
+    C_xy = (new_angular_support, sinkhorn_result.C_xy[1].detach(), None, None, None)
+    C_xx = (new_angular_support, sinkhorn_result.identity.forward(sinkhorn_result.C_xx[0].detach()), None, None, None)
     eps = sinkhorn_result.epsilon
     interpolated_potential = softmin(eps, C_xy, (sinkhorn_result.β_log + sinkhorn_result.a_y/eps)) \
         - softmin(eps, C_xx, (sinkhorn_result.α_log + sinkhorn_result.a_x/eps))
