6892913e037e6bab1ba3776c3913863a8f498079
Guillaume Chazareix <guillaume@chazareix.net>
2021-02-20 13:50:05 +0100

    Fixing gradient not propagating through interpolation

--- reflector_problem/reflector.py
+++ reflector_problem/reflector.py
@@ -21,7 +21,7 @@ def interpolate_potentials(sinkhorn_result, new_angular_support, new_ranges=None
         C_xx = (new_angular_support, sinkhorn_result.identity.forward(
             sinkhorn_result.C_xx[0].detach()))
     eps = sinkhorn_result.epsilon
-    interpolated_potential = softmin(eps, C_xy, (sinkhorn_result.β_log + sinkhorn_result.a_y/eps).detach()) \
-        - softmin(eps, C_xx, (sinkhorn_result.α_log +
-                              sinkhorn_result.a_x/eps).detach())
+    interpolated_potential = softmin(eps, C_xy, sinkhorn_result.β_log + sinkhorn_result.a_y/eps) \
+        - softmin(eps, C_xx, sinkhorn_result.α_log +
+                  sinkhorn_result.a_x/eps)
     return interpolated_potential
