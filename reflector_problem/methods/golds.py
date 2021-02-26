368310c363fc25c4ace730142ec26946e119a7c3
Guillaume Chazareix <guillaume@chazareix.net>
2021-02-26 17:28:32 +0100

    Mutipliers and history on optimization methods

--- reflector_problem/methods/golds.py
+++ reflector_problem/methods/golds.py
@@ -1,27 +1,29 @@
 from reflector_problem.point_source import compute_point_source_reflector
 from reflector_problem.raytracing.utils import to_angle
 
+
 def design_reflector_golds(
         extended_source_target,
+        extended_angular_support,
         initial_target,
-        input_angular_support,
         initial_angular_support,
-        source_description,
+        input_measure_vector,
+        input_angular_support,
         raytracer,
         binning,
+        history,
         n_steps=20,
-        lr=1.):
+        lr=1.,
+        lr_multiplier=1.):
     modified_target = initial_target.clone()
     modified_angular_support = initial_angular_support.clone()
 
-    input_measure_vector = source_description.pdf(
-        input_angular_support.detach()).squeeze(-1).cuda()
-
     input_measure_vector = input_measure_vector / input_measure_vector.sum()
-    input_angular_support = input_angular_support.to(input_measure_vector.device)
+    input_angular_support = input_angular_support.to(
+        input_measure_vector.device)
     modified_target = modified_target.to(input_angular_support.device)
-    modified_angular_support = modified_angular_support.to(input_angular_support.device)
-
+    modified_angular_support = modified_angular_support.to(
+        input_angular_support.device)
 
     for i in range(n_steps):
         sinkhorn_result = compute_point_source_reflector(
@@ -30,7 +32,7 @@ def design_reflector_golds(
             modified_target.view(-1).to(input_measure_vector.device),
             modified_angular_support.view(-1, 1)
         )
-        
+
         rays, weights = raytracer.raytrace_reflector(sinkhorn_result)
         centers, dist = binning(to_angle(rays), weights)
 
@@ -40,4 +42,12 @@ def design_reflector_golds(
 
         modified_target = modified_target/modified_target.sum(dim=-1)
 
-    return modified_target
+        history.save_step(i,
+                          modified_target=modified_target.clone().detach(),
+                          modified_angular_support=modified_angular_support.clone().detach(),
+                          rays=rays.clone().detach(),
+                          weights=weights.clone().detach())
+
+        lr = lr * lr_multiplier
+
+    return modified_target, modified_angular_support, history
--- reflector_problem/methods/gradient_descent.py
+++ reflector_problem/methods/gradient_descent.py
@@ -1,47 +1,77 @@
 from reflector_problem.point_source import compute_point_source_reflector
 from reflector_problem.raytracing.utils import to_angle
+import torch
 
 def design_reflector_gd(
         extended_source_target,
+        extended_angular_support,
         initial_target,
-        input_angular_support,
         initial_angular_support,
-        source_description,
+        input_measure_vector,
+        input_angular_support,
         raytracer,
         loss,
         optimizer,
+        history,
+        cost_normalization=True,
         n_steps=20,
-        lr=1.):
+        lr=1.,
+        lr_multiplier=1.):
     modified_target = initial_target.clone()
     modified_angular_support = initial_angular_support.clone()
 
-    modified_target_log = modified_target.log() + modified_target.logsumexp(dim=-1, keepdim=False)
+    modified_target_log = modified_target.log(
+    ) + modified_target.logsumexp(dim=-1, keepdim=False)
     modified_target_log.requires_grad_(True)
     optim = optimizer([modified_target_log],
-                      lr = lr)
+                      lr=lr)
+    scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optim, lr_lambda=lambda step:lr_multiplier)
 
-    input_measure_vector = source_description.pdf(
-        input_angular_support.detach()).squeeze(-1).cuda()
-
-    input_measure_vector = input_measure_vector / input_measure_vector.sum()
     input_angular_support = input_angular_support.to(input_measure_vector.device)
     modified_target_log = modified_target_log.to(input_angular_support.device)
     modified_angular_support = modified_angular_support.to(input_angular_support.device)
-    
-    for i in range(n_steps):
-        optim.zero_grad()
+
+    cost_normalizer = 1.
+ 
+    if cost_normalization:
         sinkhorn_result = compute_point_source_reflector(
             input_measure_vector.view(-1).to(input_measure_vector.device),
             input_angular_support.view(-1, 1),
             modified_target_log.softmax(dim=-1).view(-1).to(input_measure_vector.device),
             modified_angular_support.view(-1, 1)
         )
-        
+ 
+        rays, weights = raytracer.raytrace_reflector(sinkhorn_result)
+        cost_normalizer = loss(weights, to_angle(rays), extended_source_target, extended_angular_support)
+        cost_normalizer = cost_normalizer.detach()
+
+    for i in range(n_steps):
+        optim.zero_grad()
+        sinkhorn_result = compute_point_source_reflector(
+            input_measure_vector.view(-1).to(input_measure_vector.device),
+            input_angular_support.view(-1, 1),
+            modified_target_log.softmax(
+                dim=-1).view(-1).to(input_measure_vector.device),
+            modified_angular_support.view(-1, 1)
+        )
+
         rays, weights = raytracer.raytrace_reflector(sinkhorn_result)
 
-        cost = loss(weights, to_angle(rays), extended_source_target, modified_angular_support)
-        
-        cost.backward()
-        optim.step()
+        cost = loss(weights, to_angle(rays),
+                    extended_source_target, modified_angular_support)
+        cost = cost / cost_normalizer
 
-    return modified_target_log.softmax(dim=-1)
+        cost.backward()
+
+        optim.step()
+        scheduler.step()
+
+        history.save_step(i,
+                    modified_target=modified_target_log.softmax(dim=1).clone().detach(),
+                    modified_angular_support=modified_angular_support.clone().detach(),
+                    rays=rays.clone().detach(),
+                    weights=weights.clone().detach(),
+                    cost=cost.clone().detach())
+
+
+    return modified_target_log.softmax(dim=-1), modified_angular_support, history
